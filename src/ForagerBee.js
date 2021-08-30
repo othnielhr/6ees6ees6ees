9c721f25b796e2c2c611cbe24b9fd72a54161e59
Joseph <josephnahm1@gmail.com>
2021-08-30 13:52:31 -0700

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(color, food) {
+    super(color, food)
+    this.age = 10
+    this.job = 'find pollen'
+    this.canFly = true
+    this.treasureChest = []
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
